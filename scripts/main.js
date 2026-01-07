1ea854462da10ec7d3233510dc66a16f708b6776
Summit Fan <fank1@rose-hulman.edu>
2026-01-07 12:01:42 -0700

    changed nav bar to not require duplicated code

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const pageNamesToPages = new Map([
+	["About", "about.html"],
+	["Experience", "experience.html"],
+	["Projects", "projects.html"],
+	["Other Interests", "other_interests.html"],
+	["Resume", "resume.pdf"]
+]);
+
+function createNav(currentPageName) {
+	let name = `<div id="name"><a class="link-no-color" href="index.html">Summit Fan</a></div>`;
+	let pages = `<div class="nav-container">`;
+
+	let pageNamesIter = pageNamesToPages.keys();
+
+	let result = pageNamesIter.next();
+	while (!result.done) {
+		let pageName = result.value;
+		if (currentPageName === pageName) {
+			pages += `<div class="nav-item nav-item-selected">${pageName}</div>`;
+		} else {
+			pages += `<div class="nav-item"><a class="link-no-color" href=${pageNamesToPages.get(pageName)}>${pageName}</a></div>`;
+		}
+
+		result = pageNamesIter.next();
+		if (!result.done) {
+			pages += `<div class="nav-sep">|</div>`;
+		}
+	}
+	pages += `</div>`;
+
+	$("#navBar").html(`${name}${pages}`);
+}
